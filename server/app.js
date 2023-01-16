6cbf5a22e769a7ece75f4211ba72e29bc178a241
Donna-Sayos <Donnasayos@gmail.com>
2023-01-16 14:15:54 -0500

    added morgan logger and app listen

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+const morgan = require('morgan'); // is a middleware for Node... logger
 
-const app = express();
+const app = express(); // returns an instance of an Express Application;
 
-app.use(morgan);
+app.use(morgan('dev')); // using the 'dev' format for morgan;
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT} 🔊🔊🔊`);
+})
