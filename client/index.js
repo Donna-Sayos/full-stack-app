322bc57d87789679116085aa78561a321d37c352
Donna-Sayos <Donnasayos@gmail.com>
2023-01-24 15:01:42 -0500

    added index in client folder

---
+++ client/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// store={store} need to import store and add it to Provider
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
