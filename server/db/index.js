e4eff43e5b9e0977ca751a47e61fabff88ae97f2
Donna-Sayos <Donnasayos@gmail.com>
2023-01-23 22:14:33 -0500

    changed my postgres db file

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,13 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./server/config/config.env" });
-
+require("dotenv").config({ path: "./server/config/config.env" });
 const Pool = require("pg").Pool; // this is a class that creates a connection pool to a PostgreSQL database;
 
+// const isDevelopment = process.env.NODE_ENV;
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
 
 module.exports = pool;
