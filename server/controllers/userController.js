ebdd5b2a6398507c7beb6a7cc52ab3e8349ee02d
Donna-Sayos <Donnasayos@gmail.com>
2023-01-24 21:04:17 -0500

    fixed my user route wwith try and catch

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,12 @@
 const pool = require("../db");
 const queries = require("../db/queries/userQueries"); // this is the queries object that contains all the SQL queries;
 
-const getUsers = (req, res, next) => {
-  pool.query(queries.getUsers_, (err, results) => {
-    if (err) {
-      console.error(`Error getting all users: ${err.message}`);
-    }
-    if (results.rows.length === 0) {
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await pool.query(queries.getUsers_);
+    const noUsersFound = users.rows.length === 0;
+
+    if (noUsersFound) {
       return res.status(404).json({
         status: "fail",
         message: "No users found in the database.",
@@ -14,22 +14,28 @@ const getUsers = (req, res, next) => {
     } else {
       res.status(200).json({
         status: "success",
-        results: results.rows.length,
         data: {
-          users: results.rows,
+          users: users.rows,
         },
       });
     }
-  });
+  } catch (err) {
+    console.error(`Error getting users: ${err.message}`);
+    res.status(500).json({
+      status: "fail",
+      message: "An error occurred, please try again later.",
+    });
+  }
 };
 
-const getUserById = (req, res, next) => {
-  const ID = parseInt(req.params.id); // this is the id of the user that is being requested;
-  pool.query(queries.getUserById_, [ID], (err, results) => {
-    if (err) {
-      console.error(`Error getting user with ID ${ID}: ${err.message}`);
-    }
-    if (!results.rows[0]) {
+const getUserById = async (req, res, next) => {
+  let ID;
+  try {
+    ID = parseInt(req.params.id); // parseInt is used to convert the id string to an integer;
+    const user = await pool.query(queries.getUserById_, [ID]); // the second argument is an array of values that will be used to replace the $1, $2, $3, etc. in the SQL query;
+    const noUserFound = user.rows.length === 0;
+
+    if (noUserFound) {
       return res.status(404).json({
         status: "fail",
         message: "User does not exist in the database.",
@@ -38,145 +44,105 @@ const getUserById = (req, res, next) => {
       res.status(200).json({
         status: "success",
         data: {
-          user: results.rows,
+          user: user.rows[0], // the first element of the array is the user object;
         },
       });
     }
-  });
+  } catch (err) {
+    console.error(`Error getting user with ID ${ID}: ${err.message}`);
+    res.status(500).json({
+      status: "fail",
+      message: "An error occurred, please try again later.",
+    });
+  }
 };
 
-const addUser = (req, res, next) => {
-  const {
-    username,
-    fullname,
-    email,
-    age,
-    address,
-    dob,
-    gender,
-    hashed_password,
-    user_id,
-  } = req.body;
-  pool.query(
-    queries.checkEmailUsernameExist_,
-    [username, email],
-    (err, results) => {
-      if (err) {
-        console.error(`Error while checking if user exists: ${err.message}`);
-        return res.status(500).json({
-          status: "fail",
-          message: "An error occurred, please try again later.",
-        });
-      }
-      if (results.rows.length > 0) {
-        return res.status(409).json({
-          status: "fail",
-          message: "User already exists in the database.",
-        });
-      }
-      pool.query(
-        queries.addUser_,
-        [username, fullname, email, age, address, dob, gender, hashed_password, user_id],
-        (err, results) => {
-          if (err) {
-            console.error(`Error adding user: ${err.message}`);
-            return res.status(500).json({
-              status: "fail",
-              message: "An error occurred, please try again later.",
-            });
-          }
-          return res.status(201).json({
-            status: "success",
-            message: "User created successfully.",
-          });
-        }
-      );
-    }
-  );
-};
+const addUser = async (req, res, next) => {
+  try {
+    const { username, fullname, email, age, address, dob, gender, hashed_password } = req.body;
+    const checker = await pool.query(queries.checkEmailUsernameExist_, [
+      username,
+      email,
+    ]);
 
-const deleteUser = (req, res, next) => {
-  const ID = parseInt(req.params.id);
-  pool.query(queries.deleteUser_, [ID], (err, results) => {
-    const noUserFound = results.rowCount === 0;
-    if (err) {
-      console.error(`Error deleting user with ID ${ID}: ${err.message}`);
-      return res.status(500).json({
+    if (checker.rows.length > 0) {
+      return res.status(409).json({
         status: "fail",
-        message: "An error occurred, please try again later.",
+        message: "User exists.",
       });
-    } else if (noUserFound) {
+    } else {
+      await pool.query(queries.addUser_, [ username, fullname, email, age, address, dob, gender, hashed_password ]);
+
+      res.status(201).json({
+        status: "success",
+        message: "User added successfully.",
+      })
+    }
+  } catch (err) {
+    console.error(`Error adding user: ${err.message}`);
+    res.status(500).json({
+      status: "fail",
+      message: "An error occurred, please try again later.",
+    });
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  let ID;
+  try {
+    ID = parseInt(req.params.id);
+    const user = await pool.query(queries.getUserById_, [ID]);
+    const noUserFound = user.rows.length === 0;
+
+    if (noUserFound) {
       return res.status(404).json({
         status: "fail",
         message: "User does not exist in the database.",
       });
     } else {
+      await pool.query(queries.deleteUser_, [ID]);
       res.status(200).json({
         status: "success",
         message: "User deleted successfully.",
       });
     }
-  });
+  } catch (err) {
+    console.error(`Error deleting user with ID ${ID}: ${err.message}`);
+    res.status(500).json({
+      status: "fail",
+      message: "An error occurred, please try again later.",
+    });
+  }
 };
 
-const updateUser = (req, res, next) => {
-  const ID = parseInt(req.params.id);
-  const {
-    username,
-    fullname,
-    email,
-    age,
-    address,
-    dob,
-    gender,
-    hashed_password,
-    user_id,
-  } = req.body;
-  pool.query(queries.getUserById_, [ID], (err, results) => {
-    const noUserFound = results.rowCount === 0;
-    if (err) {
-      console.error(`Error updating user with ID ${ID}: ${err.message}`);
-      return res.status(500).json({
-        status: "fail",
-        message: "An error occurred, please try again later.",
+const updateUser = async (req, res, next) => {
+  let ID;
+  try {
+    ID = parseInt(req.params.id);
+    const { username, fullname, email, age, address, dob, gender, hashed_password, user_id } = req.body;
+    const user = await pool.query(queries.getUserById_, [ID]);
+    const noUserFound = user.rows.length === 0;
+
+   if (noUserFound) {
+    return res.status(404).json({
+      status: "fail",
+      message: "User does not exist in the database.",
+    });
+   } else {
+      await pool.query(queries.updateUser_, [ username, fullname, email, age, address, dob, gender, hashed_password, user_id, ID ]);
+
+      res.status(200).json({
+        status: "success",
+        message: "User updated successfully.",
       });
-    } else if (noUserFound) {
-      return res.status(404).json({
-        status: "fail",
-        message: "User does not exist in the database.",
-      });
-    } else {
-      pool.query(
-        queries.updateUser_,
-        [
-          username,
-          fullname,
-          email,
-          age,
-          address,
-          dob,
-          gender,
-          hashed_password,
-          user_id,
-        ],
-        (err, results) => {
-          if (err) {
-            console.error(
-              `Error updating user with ID ${ID}: ${err.message}`
-            );
-            return res.status(500).json({
-              status: "fail",
-              message: "An error occurred, please try again later.",
-            });
-          }
-          res.status(200).json({
-            status: "success",
-            message: "User updated successfully.",
-          });
-        }
-      );
-    }
-  });
+   }
+  } catch (err) {
+    console.error(`Error updating user with ID ${ID}: ${err.message}`);
+    res.status(500).json({
+      status: "fail",
+      message: "An error occurred, please try again later.",
+    });
+  }
 };
 
 module.exports = {
